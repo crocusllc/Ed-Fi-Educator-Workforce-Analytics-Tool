f9a28bd0c0008a88d4e030cd73b097f715623b7e
M-Watson <mwatson10@gmail.com>
2025-05-28 09:12:16 -0500

    updating sql scripts to create views with dbo owner

--- views/Edfi_EW_VW_Students.sql
+++ views/Edfi_EW_VW_Students.sql
@@ -12,15 +12,15 @@ SELECT
 	,r.[ShortDescription] AS RaceEthnic
 
 FROM [EdFi_Ods_Populated_Template].[edfi].[StudentSchoolAssociation] as ssa -- starting with studentSchool because it contains entry/exit dates to enable historical view
-	LEFT JOIN   [EdFi_Ods_Sandbox_oKFXKjFNu2jK].[edfi].[Student] AS stud --then adding student details for enrollment
+	LEFT JOIN   [EdFi_Ods_Populated_Template].[edfi].[Student] AS stud --then adding student details for enrollment
 		ON ssa.StudentUSI = stud.StudentUSI
-	LEFT JOIN [EdFi_Ods_Sandbox_oKFXKjFNu2jK].[edfi].[EducationOrganization] AS school --then seoa joined to school to get School Name
+	LEFT JOIN [EdFi_Ods_Populated_Template].[edfi].[EducationOrganization] AS school --then seoa joined to school to get School Name
 		on school.EducationOrganizationId = ssa.SchoolId
-	LEFT JOIN [EdFi_Ods_Sandbox_oKFXKjFNu2jK].[edfi].[School] AS SchoolLEA --now join school to get associated LEA
+	LEFT JOIN [EdFi_Ods_Populated_Template].[edfi].[School] AS SchoolLEA --now join school to get associated LEA
 		ON SchoolLEA.SchoolId = ssa.SchoolId
-	LEFT JOIN [EdFi_Ods_Sandbox_oKFXKjFNu2jK].[edfi].[EducationOrganization] AS lea --finally join seoa again to get LEA Name
+	LEFT JOIN [EdFi_Ods_Populated_Template].[edfi].[EducationOrganization] AS lea --finally join seoa again to get LEA Name
 		ON lea.EducationOrganizationId = SchoolLEA.LocalEducationAgencyId
-	LEFT JOIN  [EdFi_Ods_Sandbox_oKFXKjFNu2jK].[edfi].[StudentEducationOrganizationAssociationRace] AS studRace --get race association
+	LEFT JOIN  [EdFi_Ods_Populated_Template].[edfi].[StudentEducationOrganizationAssociationRace] AS studRace --get race association
 		ON StudRace.EducationOrganizationId = lea.EducationOrganizationId AND StudRace.StudentUSI = ssa.StudentUSI -- race per student per school
-	LEFT JOIN [EdFi_Ods_Sandbox_oKFXKjFNu2jK].[edfi].[Descriptor] as r
+	LEFT JOIN [EdFi_Ods_Populated_Template].[edfi].[Descriptor] as r
 		ON r.DescriptorId = studRace.RaceDescriptorId
