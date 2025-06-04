8634069687989663769203b2e03ade53b5c999a1
Karl Schumann <19996671+kschumann@users.noreply.github.com>
2025-06-04 14:56:53 -0400

    Initial Student/Staff combo view
    This simply creates a union of student and staff views with select columns needed for the Race/Ethnic Chart

---
+++ views/EdFi_EW_VW_StudentStaff_Ethnic.sql
@@ -0,0 +1,28 @@
+
+ With students as ( 
+ SELECT distinct
+      [StudentUSI] as ID
+      ,Campus
+      ,District
+      ,SchoolYear
+      ,RaceEthnic
+      ,'Student' as TypeOf
+  from [EdFi_Ods_Populated_Template].[dbo].[vw_Student]
+  WHERE ExitWithdrawDate is null), 
+
+staff as (
+    SELECT  distinct
+      [TeacherID] AS Id
+      ,Campus as SchoolName
+      ,District as LEAName
+      ,SchoolYear
+      ,RaceEthnic
+      ,'Staff' as TypeOf
+      from vw_Staff
+      )
+
+Select * from students
+
+union all
+
+Select * from staff
